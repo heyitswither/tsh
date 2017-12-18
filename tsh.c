9fdd901e5244d687e188819332d8c3608a1cacf2
Tucker Boniface <tucker@boniface.tech>
2017-12-17 17:42:45 -0800

    oh cool this is nice prompt now

--- tsh.c
+++ tsh.c
@@ -137,9 +137,9 @@ int tsh_status(char** args)
 
 int tsh_home(char** args)
 {
-    struct passwd* pw = getpwuid(getuid());
-    const char* homedir = pw->pw_dir;
-    printf("%s\n", homedir);
+    char* home = malloc(sizeof(char)*TSH_RL_BUFSIZE);
+    home = tsh_get_home();
+    printf("%s\n", home);
     return 0;
 }
 
@@ -191,6 +191,13 @@ int tsh_cd(char** args)
     }
   }
   return 0;
+}
+
+char* tsh_get_home(void)
+{
+    struct passwd* pw = getpwuid(getuid());
+    char* homedir = pw->pw_dir; 
+    return homedir;
 }
 
 char* tsh_get_hostname(void) {
@@ -317,11 +324,56 @@ char* tsh_read_line(void)
     }
 }
 
+char *str_replace(char *orig, char *rep, char *with) {
+    char *result; // the return string
+    char *ins;    // the next insert point
+    char *tmp = NULL;    // varies
+    int len_rep;  // length of rep (the string to remove)
+    int len_with; // length of with (the string to replace rep with)
+    int len_front; // distance between rep and end of last rep
+    int count;    // number of replacements
+
+    // sanity checks and initialization
+    if (!orig || !rep)
+        return NULL;
+    len_rep = strlen(rep);
+    if (len_rep == 0)
+        return NULL; // empty rep causes infinite loop during count
+    if (!with)
+        with = "";
+    len_with = strlen(with);
+
+    // count the number of replacements needed
+    ins = orig;
+    for (count = 0; tmp == strstr(ins, rep); ++count) {
+        ins = tmp + len_rep;
+    }
+
+    tmp = result = malloc(strlen(orig) + (len_with - len_rep) * count + 1);
+
+    if (!result)
+        return NULL;
+
+    while (count--) {
+        ins = strstr(orig, rep);
+        len_front = ins - orig;
+        tmp = strncpy(tmp, orig, len_front) + len_front;
+        tmp = strcpy(tmp, with) + len_with;
+        orig += len_front + len_rep; // move to next "end of rep"
+    }
+    strcpy(tmp, orig);
+    return result;
+}
+
 int tsh_loop(void) {
     char* line;
     char** args;
+    char* pwd = NULL;
+    char* tmp = NULL;
     do {
-        printf("> ");
+        tmp = tsh_get_pwd();
+        pwd = str_replace(tmp, tsh_get_home(), "~");
+        printf("%s@%s:%s%s ", tsh_get_user(), tsh_get_hostname(), pwd, tsh_get_euid() == 0 ? "#" : "$");
         line = tsh_read_line();
         args = tsh_split_line(line);
         tsh_status_code = tsh_execute(args);
