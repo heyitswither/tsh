4029854336c3c4f9c50dda763b5c68637c283112
Tucker Boniface <tucker@boniface.tech>
2017-12-17 16:54:56 -0800

    lots of things added

--- tsh.c
+++ tsh.c
@@ -11,14 +11,18 @@
 #define TSH_TOK_BUFSIZE 64
 #define TSH_TOK_DELIM " \t\r\n\a"
 
-int tsh_loop(void);
-char* tsh_read_line(void);
+int tsh_status_code = 0;
+
 char** tsh_split_line(char* line);
 int tsh_launch(char** args);
-int tsh_num_builtins(void);
 int tsh_execute(char** args);
+int tsh_loop(void);
+char* tsh_read_line(void);
+int tsh_num_builtins(void);
 char* tsh_get_home(void);
 char* tsh_get_pwd(void);
+char* tsh_get_user(void);
+void tsh_clean_exit(void);
 
 /* builtin shell commands */
 int tsh_cd(char** args);
@@ -29,19 +33,26 @@ int tsh_home(char** args);
 int tsh_true(char** args);
 int tsh_false(char** args);
 int tsh_exec(char** args);
+int tsh_status(char** args);
+int tsh_user(char** args);
 
-char *builtin_str[] = {
-  "cd",
-  "help",
-  "exit",
-  "pwd",
-  "home",
-  "true",
-  "false",
-  "exec"
+char* builtin_str[] = {
+  "cd", "help", "exit", "pwd", "home", "true", "false", "exec", "status", "user"
+};
+char* builtin_desc[] = {
+  "changes the current directory",
+  "shows this message",
+  "exits the shell",
+  "prints the current directory",
+  "prints the path to home",
+  "returns true",
+  "returns false",
+  "replaces the current process",
+  "prints or changes the status",
+  "prints the current logged in user"
 };
 
-int (*builtin_func[]) (char **) = {
+int (*builtin_func[]) (char**) = {
   &tsh_cd,
   &tsh_help,
   &tsh_exit,
@@ -49,11 +60,13 @@ int (*builtin_func[]) (char **) = {
   &tsh_home,
   &tsh_true,
   &tsh_false,
-  &tsh_exec
+  &tsh_exec,
+  &tsh_status,
+  &tsh_user
 };
 
 int tsh_num_builtins(void) {
-  return sizeof(builtin_str) / sizeof(char *);
+  return sizeof(builtin_str) / sizeof(char*);
 }
 
 int main(/*int argc, char** argv*/ void) {
@@ -63,7 +76,14 @@ int main(/*int argc, char** argv*/ void) {
     int success = tsh_loop();
 
     // exit cleanly, freeing everything
+    tsh_clean_exit();   
+
     return success;
+}
+
+void tsh_clean_exit(void)
+{
+    ;
 }
 
 int tsh_execute(char **args)
@@ -84,25 +104,47 @@ int tsh_execute(char **args)
   return tsh_launch(args);
 }
 
+int tsh_user(char** args)
+{
+    char* user = malloc(sizeof(char)*TSH_RL_BUFSIZE);
+    user = tsh_get_user();
+    printf("%s\n", user);
+    return 0;
+}
+
+int tsh_status(char** args)
+{
+    if (args[1] == NULL) {
+        printf("%d\n", tsh_status_code);
+    } else {
+        int new_code = 0;
+        sscanf(args[1], "%d", &new_code);
+        tsh_status_code = new_code;
+        printf("Status set to %d\n", new_code);
+        return new_code;
+    }
+    return 0;
+}
+
 int tsh_pwd(char** args)
 {
     char* cwd = malloc(TSH_RL_BUFSIZE);
     cwd = tsh_get_pwd();
     printf("%s\n", cwd);
-    return 1;
+    return 0;
 }
 
 int tsh_home(char** args)
 {
-    struct passwd *pw = getpwuid(getuid());
-    const char *homedir = pw->pw_dir;
+    struct passwd* pw = getpwuid(getuid());
+    const char* homedir = pw->pw_dir;
     printf("%s\n", homedir);
-    return 1;
+    return 0;
 }
 
-int tsh_exit(char **args)
+int tsh_exit(char** args)
 {
-  return 0;
+  return -1;
 }
 
 int tsh_true(char** args)
@@ -120,10 +162,10 @@ int tsh_exec(char** args)
     if (execvp(args[0], args) == -1) {
         perror("tsh");
     }    
-    return EXIT_FAILURE;
+    return 1;
 }
 
-int tsh_help(char **args)
+int tsh_help(char** args)
 {
   int i;
   printf("Tucker Boniface's TSH\n");
@@ -131,14 +173,14 @@ int tsh_help(char **args)
   printf("The following are built in:\n");
 
   for (i = 0; i < tsh_num_builtins(); i++) {
-    printf("  %s\n", builtin_str[i]);
+    printf("  %s - %s\n", builtin_str[i], builtin_desc[i]);
   }
 
   printf("Use the man command for information on other programs.\n");
-  return 1;
+  return 0;
 }
 
-int tsh_cd(char **args)
+int tsh_cd(char** args)
 {
   if (args[1] == NULL) {
     fprintf(stderr, "tsh: expected argument to \"cd\"\n");
@@ -147,7 +189,7 @@ int tsh_cd(char **args)
       perror("tsh");
     }
   }
-  return 1;
+  return 0;
 }
 
 char* tsh_get_pwd(void) {
@@ -159,7 +201,14 @@ char* tsh_get_pwd(void) {
     return cwd;
 }
 
-int tsh_launch(char **args)
+char* tsh_get_user(void)
+{
+    char* user = malloc(sizeof(char)*TSH_RL_BUFSIZE);
+    user = getlogin();
+    return user;
+}
+
+int tsh_launch(char** args)
 {
     pid_t pid;
     int status;
@@ -187,8 +236,8 @@ int tsh_launch(char **args)
 char** tsh_split_line(char *line)
 {
     int bufsize = TSH_TOK_BUFSIZE, position = 0;
-    char **tokens = malloc(bufsize * sizeof(char*));
-    char *token;
+    char** tokens = malloc(bufsize * sizeof(char*));
+    char* token;
 
     if (!tokens) {
         fprintf(stderr, "tsh: allocation error\n");
@@ -219,7 +268,7 @@ char* tsh_read_line(void)
 {
     int bufsize = TSH_RL_BUFSIZE;
     int position = 0;
-    char *buffer = malloc(sizeof(char) * bufsize);
+    char* buffer = malloc(sizeof(char) * bufsize);
     int c;
 
     if (!buffer) {
@@ -256,16 +305,17 @@ char* tsh_read_line(void)
 }
 
 int tsh_loop(void) {
-    char *line;
-    char **args;
-    int status;
+    char* line;
+    char** args;
     do {
         printf("> ");
         line = tsh_read_line();
         args = tsh_split_line(line);
-        status = tsh_execute(args);
+        tsh_status_code = tsh_execute(args);
+        if (tsh_status_code < -1)
+            return EXIT_FAILURE;
         free(line);
         free(args);
-    } while (status);
+    } while (tsh_status_code != -1);
     return EXIT_SUCCESS;
 }
